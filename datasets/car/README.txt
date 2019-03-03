f36b55c979e0d03f38781a2d20e56a59e049abb0
Brad Reardon <brad.jay.reardon@gmail.com>
2019-03-02 19:26:01 -0500

    working(?) code modifications for abalone test to use cancer data

--- datasets/car/README.txt
+++
@@ -1,1 +0,0 @@
-Source: https://archive.ics.uci.edu/ml/datasets/car+evaluation
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 export CLASSPATH=abagail/ABAGAIL.jar:$CLASSPATH
-mkdir -p out/{}
+mkdir -p out/{error,log}
+
+echo "abalone_test_cancer"
+jython randomized_optimization/abalone_test_cancer.py
